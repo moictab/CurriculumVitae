01635f09798ec25ca4be43fb8669f0b674ad8371
Moisés <moictab@gmail.com>
2019-03-11 13:30:56 +0100

    Clean code

--- app/src/main/java/com/moictab/curriculumvitae/fragments/ContactFragment.kt
+++ app/src/main/java/com/moictab/curriculumvitae/fragments/ContactFragment.kt
@@ -6,45 +6,47 @@ import android.content.pm.PackageManager
 import android.net.Uri
 import android.os.Bundle
 import android.support.v4.app.Fragment
+import android.util.Log
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.ImageButton
 import android.widget.Toast
-import butterknife.ButterKnife
 import com.moictab.curriculumvitae.R
-import kotlinx.android.synthetic.main.fragment_contact.*
 
 class ContactFragment : Fragment() {
-  private var mListener: OnFragmentInteractionListener? = null
+  private var onFragmentInteractionListener: OnFragmentInteractionListener? = null
 
   override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?): View? {
     val view = inflater.inflate(R.layout.fragment_contact, container, false)
-    ButterKnife.bind(this, view)
     activity?.title ?: getString(R.string.contact_fragment_title)
 
-    btn_email.setOnClickListener {
+    val btnEmail = view.findViewById<ImageButton>(R.id.btn_email)
+    btnEmail.setOnClickListener {
       val intent = Intent(Intent.ACTION_SEND)
       intent.type = "message/rfc822"
       intent.putExtra(Intent.EXTRA_EMAIL, arrayOf("xxx@gmail.com"))
       try {
         startActivity(Intent.createChooser(intent, "Send email..."))
-      } catch (ex: android.content.ActivityNotFoundException) {
+      } catch (e: android.content.ActivityNotFoundException) {
+        Log.e(TAG, "Error getting activity for sending emails", e)
         Toast.makeText(activity, R.string.no_email_clients_installed, Toast.LENGTH_SHORT).show()
       }
     }
 
-    btn_linkedin.setOnClickListener {
+    val btnLinkedIn = view.findViewById<ImageButton>(R.id.btn_linkedin)
+    btnLinkedIn.setOnClickListener {
       var intent = Intent(Intent.ACTION_VIEW, Uri.parse("linkedin://moictab"))
       val packageManager = context!!.packageManager
       val list = packageManager.queryIntentActivities(intent, PackageManager.MATCH_DEFAULT_ONLY)
       if (list.isEmpty()) {
         intent = Intent(Intent.ACTION_VIEW, Uri.parse("http://www.linkedin.com/profile/view?id=moictab"))
       }
-
       startActivity(intent)
     }
 
-    btn_phone!!.setOnClickListener {
+    val btnPhone = view.findViewById<ImageButton>(R.id.btn_phone)
+    btnPhone!!.setOnClickListener {
       val intent = Intent(Intent.ACTION_DIAL, Uri.fromParts("tel", "XXXXXXXXX", null))
       startActivity(intent)
     }
@@ -52,17 +54,10 @@ class ContactFragment : Fragment() {
     return view
   }
 
-  // TODO: Rename method, update argument and hook method into UI event
-  fun onButtonPressed(uri: Uri) {
-    if (mListener != null) {
-      mListener!!.onFragmentInteraction(uri)
-    }
-  }
-
   override fun onAttach(context: Context?) {
     super.onAttach(context)
     if (context is OnFragmentInteractionListener) {
-      mListener = context
+      onFragmentInteractionListener = context
     } else {
       throw RuntimeException(context!!.toString() + " must implement OnFragmentInteractionListener")
     }
@@ -70,26 +65,14 @@ class ContactFragment : Fragment() {
 
   override fun onDetach() {
     super.onDetach()
-    mListener = null
+    onFragmentInteractionListener = null
   }
 
-
   interface OnFragmentInteractionListener {
-    // TODO: Update argument type and name
     fun onFragmentInteraction(uri: Uri)
   }
 
   companion object {
-    private val ARG_PARAM1 = "param1"
-    private val ARG_PARAM2 = "param2"
-
-    fun newInstance(param1: String, param2: String): ContactFragment {
-      val fragment = ContactFragment()
-      val args = Bundle()
-      args.putString(ARG_PARAM1, param1)
-      args.putString(ARG_PARAM2, param2)
-      fragment.arguments = args
-      return fragment
-    }
+    const val TAG = "ContactFragment"
   }
 }
